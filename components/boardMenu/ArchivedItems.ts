17cbc5d09cfd0b676e66232902b4535e242ca282
shacooltchy <72758785+shacooltchy@users.noreply.github.com>
2026-04-08 12:08:38 +0200

    Unarchive a list test (#22)

--- components/boardMenu/ArchivedItems.ts
+++ components/boardMenu/ArchivedItems.ts
@@ -12,6 +12,10 @@ export class ArchivedItems {
         await expect(this.archivedItemsPopover).toBeVisible();
     }
 
+    async expectPopoverIsNotVisible(): Promise<void> {
+        await expect(this.archivedItemsPopover).not.toBeVisible();
+    }
+
     async viewArchivedItems(option: 'Lists' | 'Cards'): Promise<void> {
         await expect(this.archivedItemsPopover.getByTestId('board-menu-container').getByRole('button', {name: 'Lists'}).or(this.archivedItemsPopover.getByTestId('board-menu-container').getByRole('button', {name: 'Cards'}))).toBeVisible();
         const switchListsCardsButton = this.archivedItemsPopover.getByTestId('board-menu-container').getByRole('button', {name: option});
@@ -25,11 +29,19 @@ export class ArchivedItems {
         await expect(this.archivedItemsPopover.getByTestId('board-menu-container').locator('div').getByText(itemName, {exact: true})).toBeVisible();
     }
 
+    async expectItemIsNotArchived(itemName: string) {
+        await expect(this.archivedItemsPopover.getByTestId('board-menu-container').locator('div').getByText(itemName, {exact: true})).not.toBeVisible();
+    }
+
     async restoreItem(itemName: string) {
         await this.archivedItemsPopover.getByTestId('board-menu-container').locator('div').filter({has: this.page.getByText(itemName, {exact: true})}).getByRole('button', {name: 'Restore'}).click();
     }
 
     async deleteItem(itemName: string) {
         await this.archivedItemsPopover.getByTestId('board-menu-container').locator('div').filter({has: this.page.getByText(itemName, {exact: true})}).getByRole('button').filter({has: this.page.getByTestId('TrashIcon')}).click();
+    }
+
+    async close(): Promise<void> {
+        await this.archivedItemsPopover.getByRole('button', { name: 'Close popover' }).click();
     }
 }
