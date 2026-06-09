fe779a41297e58c61c44236601b85846c122716d
Łukasz Zychowski <lukaszzychowski@icloud.com>
2026-06-09 20:52:20 +0200

    Change board visibility test

---
+++ tests/authenticated/boards/changeVisibility.spec.ts
@@ -0,0 +1,71 @@
+import { BoardMenuVisibilityStatus } from "../../../components/board/boardMenu/boardPopoverMenu";
+import { ChangeVisibilityPopoverOption } from "../../../components/board/boardMenu/changeVisibility";
+import { BoardMenuOption } from "../../../enums/boardMenuOption";
+import { BoardVisibility } from "../../../enums/boardVisibility";
+import { test } from "../../../fixtures/pages";
+import { createBoard } from "../../../helpers/api/boards/createBoard";
+import { deleteTestBoard } from "../../../helpers/testDataHelpers/deleteTestBoard";
+import { randomName } from "../../../utils/stringUtils";
+
+test.describe('Change board visibility tests', {tag: '@boards'}, () => {
+    test.use({ storageState: 'playwright/.auth/user.json'});
+    let boardName: string;
+
+    test.beforeEach(async ({page, boardDetailsPage }) => {
+        //create a board via API and navigate to boards page
+        boardName = randomName('Board');
+        let board = await createBoard(boardName);
+        // navigate to board details page
+        await page.goto(board.url);
+        await boardDetailsPage.expectPageVisible(boardName);
+        await boardDetailsPage.adBanner.minimizeIfVisible();
+    });
+
+    test.afterEach(async ({ page }) => {
+        //delete the created board via API
+        await deleteTestBoard(boardName);
+    });
+
+    test('Change board visibility to workspace', async ({ boardDetailsPage, page }) => {
+        await test.step('Open board menu', async () => {
+            await boardDetailsPage.clickOnBoardMenuButton();
+            await boardDetailsPage.boardMenu.expectMenuToBeVisible();
+        });
+
+        await test.step('Click visibility option', async () => {
+            await boardDetailsPage.boardMenu.clickOption(BoardMenuOption.Visibility);
+        });
+
+        await test.step('Verify visibility popover is visible', async () => {
+            await boardDetailsPage.boardMenu.changeVisibilityPopover.expectVisible();
+        });
+
+        await test.step('Verify workspace option not selected', async () => {
+            await boardDetailsPage.boardMenu.changeVisibilityPopover.expectVisibilityNotSelected(ChangeVisibilityPopoverOption.Workspace);
+        });
+
+        await test.step('Change visibility to workspace', async () => {
+            await boardDetailsPage.boardMenu.changeVisibilityPopover.selectVisibility(ChangeVisibilityPopoverOption.Workspace);
+        });
+
+        await test.step('Verify visibility is changed to workspace', async () => {
+            await boardDetailsPage.boardMenu.changeVisibilityPopover.expectVisibilitySelected(ChangeVisibilityPopoverOption.Workspace);
+        });
+
+        await test.step('Close visibility popover', async () => {
+            await boardDetailsPage.boardMenu.changeVisibilityPopover.closeButton.click();
+            await boardDetailsPage.boardMenu.changeVisibilityPopover.expectNotVisible();
+        });
+
+        await test.step('Verify board visibility is updated in the board menu', async () => {
+            await boardDetailsPage.boardMenu.expectVisibilitySelected(BoardMenuVisibilityStatus.Workspace);
+        });
+
+        await test.step('Refresh the page and verify visibility is still workspace', async () => {
+            await page.reload();
+            await boardDetailsPage.expectPageVisible(boardName);
+            await boardDetailsPage.clickOnBoardMenuButton();
+            await boardDetailsPage.boardMenu.expectVisibilitySelected(BoardMenuVisibilityStatus.Workspace);
+        });
+    });
+});
